3e9ce5bf37acd00dc80a76640f8391f3a1bc3d8c
SwissCheese15 <110164826+SwissCheese15@users.noreply.github.com>
2022-11-02 18:41:20 +0100

    added new frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import store from "./redux/Store";
+// import store from "./redux/Store";
 import { GlobalStyle } from "./styles/global.styles";
 import ShopSection from "./components/ShopSection";
 import ProductPage from "./components/ShopSection/ProductPage/ProductPage";
 import Cart from "./components/ShopSection/Cart";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
+root.render(<>
+  // <Provider store={store}>
     <GlobalStyle />
     <BrowserRouter>
       <Routes>
@@ -23,7 +23,7 @@ root.render(
         <Route path="/shop/cart" element={<Cart/>} />
       </Routes>
     </BrowserRouter>
-  </Provider>
+  // </Provider></>
 );
 
 // If you want to start measuring performance in your app, pass a function
