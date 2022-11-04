0babc13bae8ce8ea470bcf7410c2272c06b0d769
Sıbel GAROFALO <sblgrfl@gmail.com>
2022-11-04 15:53:32 +0100

    Routing, Login screen, Register screen layouts

---
+++ frontend/src/components/LoginSection/Login/Login.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledLoginContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 39px;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
