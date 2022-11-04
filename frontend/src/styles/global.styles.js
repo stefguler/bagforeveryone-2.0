64359474e417412a6d71bd5fcdeaaed3890225bc
Stef <stef.guler@gmail.com>
2022-11-04 10:53:25 +0100

    fixing global styles

--- frontend/src/styles/global.styles.js
+++ frontend/src/styles/global.styles.js
@@ -3,5 +3,15 @@ import { createGlobalStyle } from "styled-components";
 // ------------------- GLOBAL STYLES ------------------
 
 export const GlobalStyle = createGlobalStyle`
-
-`
+    body, #root {
+        font-family: 'Montserrat', sans-serif;
+        background: white;
+        display: flex;
+        align-items: center;
+        margin: 0 auto;
+        padding: 0;
+        box-sizing: border-box;
+        width: 100%;
+        height: 100vh;
+    }
+`;
