0a3f07ddfa18489a005c3be26e6a8bea5d1f6cea
Stef <stef.guler@gmail.com>
2022-11-07 12:25:41 +0100

    updates on checkout

--- frontend/src/components/ShopSection/Checkout/Checkout.js
+++ frontend/src/components/ShopSection/Checkout/Checkout.js
@@ -1,18 +1,83 @@
 // import { useNavigate } from "react-router-dom";
 
-import {CheckoutContainer} from "./Checkout.styled.js";
+import {CheckoutContainer, CheckoutHeader, CheckoutForm, FormTitle, ShippingForm} from "./Checkout.styled.js";
+import { useState } from "react";
 
-export default function Checkout(props) {
+
+export default function Checkout() {
   // const navigate = useNavigate();
+  //form states
+  const [email, setEmail] = useState()
+  const [first_name, setFirstName] = useState()
+  const [last_name, setLastName] = useState()
+  const [street, setStreet] = useState()
+  const [street_number, setStreetNumber] = useState()
+  const [zip, setZip] = useState()
+  const [city, setCity] = useState()
+  const [country, setCountry] = useState()
+  const [phone, setPhone] = useState()
 
+
+const handleInputChange = (inputField, data) =>
+{
+  switch (inputField) {
+    case "email": 
+      setEmail(data);      
+      break;    
+    case "first_name": 
+      setFirstName(data);      
+      break;
+    case "last_name": 
+      setLastName(data);      
+      break;
+    case "street": 
+      setStreet(data);      
+      break;    
+    case "street_number": 
+      setStreetNumber(data);      
+      break;
+    case "zip": 
+      setZip(data);      
+      break;    
+    case "city": 
+      setCity(data);      
+      break;
+    case "country": 
+      setCountry(data);      
+      break;
+    case "phone": 
+      setPhone(data);      
+      break;  
+
+    default:
+      break;
+  }
+} 
+
+const newFun = () => {
+  
+}
+
+
+const handleOrderSubmit = (e) =>
+{
+  e.preventDefault();
+  console.log('order submitted')
+}
 
   return (
     <>
       <CheckoutContainer >
-        {/* <CheckoutHeader>Checkout</CheckoutHeader> */}
-
-
+        <CheckoutHeader>Checkout</CheckoutHeader>
+        <p>checkout implementation</p>
+        <CheckoutForm onSubmit={(e) => handleOrderSubmit(e)}>
+          <FormTitle>1. Shipping</FormTitle>
+          <ShippingForm>
+            
+          </ShippingForm>
         
+        
+        </CheckoutForm>     
       </CheckoutContainer>
     </>
   );
--- frontend/src/components/ShopSection/Checkout/Checkout.styled.js
+++ frontend/src/components/ShopSection/Checkout/Checkout.styled.js
@@ -2,5 +2,22 @@ import styled from "styled-components";
 
 
 export const CheckoutContainer = styled.div`
+    margin-top: 8rem;
+
+`
+
+export const CheckoutHeader = styled.div`
+
+`
+
+export const CheckoutForm = styled.form`
+
+`
+
+export const FormTitle = styled.div`
+    font-weight: bold;
+`
+
+export const ShippingForm = styled.div`
 
 `
