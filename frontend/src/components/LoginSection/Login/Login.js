b1092f8bde695635288e8057bc8f61733eb3cf0f
Sıbel GAROFALO <sblgrfl@gmail.com>
2022-11-07 06:44:30 +0100

    Login Layout

--- frontend/src/components/LoginSection/Login/Login.js
+++ frontend/src/components/LoginSection/Login/Login.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+
 import {
     StyledButton,
     StyledInput,
     StyledLoginSectionContainer,
-    StyledText
+    StyledText,
+    ErrorContainer
 } from '../LoginSection.styled';
 
 import {
@@ -14,15 +17,41 @@ import {
 } from 'react-router-dom';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const onSubmit = (args) => {
+        args.preventDefault();
+    
+        if (email.length < 1) {
+            setError('Please enter your email.');
+            return;
+        }
+        if (password.length < 1) {
+            setError('Please enter your password.');
+            return;
+        }     
+        setError('');
+    }
+
     return (
         <StyledLoginSectionContainer>
-            <StyledLoginContainer>
-                <StyledInput type="email" placeholder="Email" />
-                <StyledInput type="password" placeholder="Password" />
-                <StyledButton>LOG IN</StyledButton>
+            <StyledLoginContainer onSubmit={onSubmit}>
+                <StyledInput type="email" placeholder="Email" value={email} onChange={(args) => setEmail(args.target.value)}/>
+                <StyledInput type="password" placeholder="Password" value={password} onChange={(args) => setPassword(args.target.value)} />
+                <StyledButton  type='submit'>LOG IN</StyledButton>
+                
                 <StyledText>
                     Not signed up yet? <NavLink to="/register">Sign up</NavLink>
                 </StyledText>
+                {
+                    error.length > 0 &&
+                    <ErrorContainer>
+                        {error}
+                    </ErrorContainer>
+                }
             </StyledLoginContainer>
         </StyledLoginSectionContainer>
     )
