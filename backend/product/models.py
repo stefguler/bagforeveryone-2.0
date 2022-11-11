d9507e128a1ee3d3a58fc90cc5b70ee1ec606ce7
SwissCheese15 <110164826+SwissCheese15@users.noreply.github.com>
2022-11-11 14:13:57 +0100

    pulling stefs master

--- backend/product/models.py
+++ backend/product/models.py
@@ -21,6 +21,15 @@ class Product(models.Model):
     def product_image_directory_path(instance, filename):
         return f'product/{instance.id}/{filename}'
 
+    def product_sub_image_1_path(instance, filename):
+        return f'product/{instance.id}/sub1/{filename}'
+
+    def product_sub_image_2_path(instance, filename):
+        return f'product/{instance.id}/sub2/{filename}'
+
+    def product_sub_image_3_path(instance, filename):
+        return f'product/{instance.id}/sub3/{filename}'
+
     name = models.CharField(max_length=250, unique=True)
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
     description = models.CharField(max_length=250)
@@ -31,3 +40,7 @@ class Product(models.Model):
     image = models.ImageField(blank=True, upload_to=product_image_directory_path)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    sub_image_1 = models.ImageField(blank=True, upload_to=product_sub_image_1_path)
+    sub_image_2 = models.ImageField(blank=True, upload_to=product_sub_image_2_path)
+    sub_image_3 = models.ImageField(blank=True, upload_to=product_sub_image_3_path)
+    stripe_price = models.CharField(max_length=75, blank=True)
