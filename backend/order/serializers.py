5c66a47b5582cdd78d55bff34ca12c53f93d8069
SwissCheese15 <110164826+SwissCheese15@users.noreply.github.com>
2022-11-14 16:24:57 +0100

    adrien help

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,22 +1,39 @@
 # from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from order.models import Order
+from order.models import Order, ProductType
+from product.models import Product
 from users.serializers import UserSerializer
 
 # User = get_user_model()
 
 
+# class OrderSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         model = Order
+#         fields = "__all__"
+#
+#     # def to_representation(self, instance):
+#     #     representation = super().to_representation(instance)
+#     #     representation['owner'] = RepresentationUserSerializer(instance.owner, many=False).data
+#     #     return representation
+#
+class OrderProductSerializer(serializers.ModelSerializer):
+    type = serializers.PrimaryKeyRelatedField(read_only=False, required=True, queryset=Product.objects.all())
+
+    class Meta:
+        model = ProductType
+        fields = ["type"]
+
 class OrderSerializer(serializers.ModelSerializer):
+    products = OrderProductSerializer(many=True, source='producttype_set')
 
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = ["id", "status", "content", "buyer", "created", "updated", "email", "first_name", "last_name", "street", "street_number", "zip", "city", "country", "phone", "shipping_note", "products"]
 
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['owner'] = RepresentationUserSerializer(instance.owner, many=False).data
-    #     return representation
+
 
 
 class NewOrderSerializer(serializers.ModelSerializer):
